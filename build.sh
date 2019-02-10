b231752a2eb1a00ec97c3f2cb7728efd9ec59dac
bob <bob@lavono.lavono>
2019-02-10 18:28:21 +0000

    working external interrupt

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-avr-gcc -Os -DF_CPU=16000000UL -mmcu=atmega328p -c -o led.o led.c
-avr-gcc -mmcu=atmega328p led.o -o led
-avr-objcopy -O ihex -R .eeprom led led.hex
-avrdude -F -V -c arduino -p ATMEGA328P -P /dev/ttyACM0 -b 115200 -U flash:w:led.hex
+avr-gcc -Os -DF_CPU=16000000UL -mmcu=atmega328p -c -o firm.o firm.c
+avr-gcc -mmcu=atmega328p firm.o -o firm
+avr-objcopy -O ihex -R .eeprom firm firm.hex
+avrdude -F -V -c arduino -p ATMEGA328P -P /dev/ttyACM0 -b 115200 -U flash:w:firm.hex
